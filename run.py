965b7dd1e3b6a7f3c69d8bdcea03930ce09a3a9d
pbokey <pbokey@gmail.com>
2017-10-21 22:23:01 -0500

    changed Procfile

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from website_package import website
+import website_package
 from alexa_package import alexa
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ def run_app():
 	app.register_blueprint(website)
 	app.register_blueprint(alexa)
 	app.run(debug=True)
+	return rederict("https://secure-sands-99264.herokuapp.com/website")
 
 if __name__ == "__main__":
 	run_app()
