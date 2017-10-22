34c5e41ceb681c5c0db58e19970cda5b3e6d8407
pbokey <pbokey@gmail.com>
2017-10-21 22:40:09 -0500

    changed port routing

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from website_package import website
 import website_package
 from alexa_package import alexa
@@ -8,8 +9,8 @@ app = Flask(__name__)
 def run_app():
 	app.register_blueprint(website)
 	app.register_blueprint(alexa)
-	app.run(debug=True)
-	return url_for('website.index')
+	port = int(os.environ.get('PORT', 5000))
+	app.run(debug=True, port=port)
 
 
 if __name__ == "__main__":
