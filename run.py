09a20a23f194b4852f8808de63f847a7aa6bbf05
pbokey <pbokey@gmail.com>
2017-10-21 22:18:48 -0500

    changed Procfile

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ from website_package import website
 from alexa_package import alexa
 
 app = Flask(__name__)
-app.register_blueprint(website)
-app.register_blueprint(alexa)
-app.run(debug=True)
+
+def run_app():
+	app.register_blueprint(website)
+	app.register_blueprint(alexa)
+	app.run(debug=True)
+
+if __name__ == "__main__":
+	run_app()
