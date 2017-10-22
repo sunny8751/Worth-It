e0448fa4e1b883c35b08e2cb54a104374445403b
pbokey <pbokey@gmail.com>
2017-10-21 22:41:54 -0500

    changed port routing

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def run_app():
 	app.register_blueprint(website)
 	app.register_blueprint(alexa)
 	port = int(os.environ.get('PORT', 5000))
-	app.run(debug=True, port=port)
+	app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == "__main__":
